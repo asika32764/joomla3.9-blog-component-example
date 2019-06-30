384154a7b745358d2d2a9a2c84274c5fb25de803
Simon Asika <asika32764@gmail.com>
2019-06-30 12:54:46 +0800

    在 Template 內使用動態變數

--- admin/views/example/tmpl/default.php
+++ admin/views/example/tmpl/default.php
@@ -1,4 +1,5 @@
 <?php
 defined('_JEXEC') or die;
 ?>
-<h1>Example Template</h1>
+<h1><?php echo $this->title; ?></h1>
+<p><?php echo $this->content; ?> in <?php echo $this->date->format('Y-m-d', true); ?></p>
--- admin/views/example/view.html.php
+++ admin/views/example/view.html.php
@@ -1,5 +1,6 @@
 <?php
 
+use Joomla\CMS\Date\Date;
 use Joomla\CMS\MVC\View\HtmlView;
 
 defined('_JEXEC') or die;
@@ -8,6 +9,10 @@ class BlogViewExample extends HtmlView
 {
     public function display($tpl = null)
     {
+        $this->title = 'Example View & Template';
+        $this->content = 'Go go let\'s go~~~!!!!';
+        $this->date = new Date('now', 'Asia/Taipei');
+
         parent::display($tpl);
     }
 }
