c5218d2fa8a92125d269c6924992257ea358bd59
Simon Asika <asika32764@gmail.com>
2019-06-30 13:27:31 +0800

    建立表單

--- admin/views/article/tmpl/edit.php
+++ admin/views/article/tmpl/edit.php
@@ -1,4 +1,54 @@
 <?php
 defined('_JEXEC') or die;
 ?>
-<h1>Article Edit</h1>
+<form action="<?php echo JUri::getInstance(); ?>" id="adminForm" name="adminForm" method="post">
+    <fieldset class="form-horizontal">
+        <legend>Blog Info</legend>
+
+        <!-- Title -->
+        <div class="control-group">
+            <label for="form-title" class="control-label">Title</label>
+            <div class="controls">
+                <input type="text" id="form-title" name="title" value="" />
+            </div>
+        </div>
+
+        <!-- Alias -->
+        <div class="control-group">
+            <label for="form-alias" class="control-label">Alias</label>
+            <div class="controls">
+                <input type="text" id="form-alias" name="alias" value="" />
+            </div>
+        </div>
+
+        <!-- Created -->
+        <div class="control-group">
+            <label for="form-created" class="control-label">Created Time</label>
+            <div class="controls">
+                <?php echo JHtml::calendar('now', 'created', 'form-created'); ?>
+            </div>
+        </div>
+    </fieldset>
+
+    <fieldset>
+        <legend>Text</legend>
+
+        <!-- Intro text -->
+        <div class="control-group row-fluid">
+            <label for="form-title" class="control-label">Intro Text</label>
+            <div class="controls span6">
+                <?php echo $this->introEditor; ?>
+            </div>
+        </div>
+
+        <hr />
+
+        <!-- Full text -->
+        <div class="control-group row-fluid">
+            <label for="form-alias" class="control-label">Full text</label>
+            <div class="controls span6">
+                <?php echo $this->fullEditor; ?>
+            </div>
+        </div>
+    </fieldset>
+</form>
--- admin/views/article/view.html.php
+++ admin/views/article/view.html.php
@@ -1,5 +1,6 @@
 <?php
 
+use Joomla\CMS\Editor\Editor;
 use Joomla\CMS\MVC\View\HtmlView;
 
 defined('_JEXEC') or die;
@@ -8,6 +9,13 @@ class BlogViewArticle extends HtmlView
 {
     public function display($tpl = null)
     {
+        // 取得全站設定中的編輯器設定檔
+        $config = JFactory::getConfig();
+
+        // 呼叫編輯器物件，直接 render 出來
+        $this->introEditor = Editor::getInstance($config->get('editor'))->display('introtext', '', '600px', '300px', 50, 15);
+        $this->fullEditor = Editor::getInstance($config->get('editor'))->display('fulltext', '', '600px', '300px', 50, 15);
+
         $this->addToolbar();
 
         parent::display($tpl);
